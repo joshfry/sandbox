726106fe1e99425eddf3e2cfa65d9af830d6a941
joshfry <joshdfry@me.com>
2022-10-12 18:11:11 -0600

    Add an image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg'
 import './App.css'
+import mountainImage from './assets/images/mountains.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="backgoundImage" style={{ backgroundImage: `url(${mountainImage})` }} /> */}
+      <div className="container">
+        <img src={mountainImage} alt="Mountains" />
+      </div>
     </div>
   )
 }
